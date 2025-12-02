7c02a24cde85962cd71920ba2298c02b922385c1
vickbk <vickbake8@gmail.com>
2025-12-02 18:08:49 +0200

    updating some styles properties

--- src/components/mortageCalculator/calculator/CalculatorForm.tsx
+++ src/components/mortageCalculator/calculator/CalculatorForm.tsx
@@ -63,7 +63,6 @@ export const CalculatorForm = ({
       <button
         className="flex justify-center gap-4 p-4 rounded-x-full font-bold lime transition-transform duration-300 active:scale-95 border-2 b-white out-lime  hover:outline-2 focus:scale-105 focus:outline-0 md:place-self-start md:px-12"
         type="submit"
-        style={{ "--bg-accent": 0.7 } as React.CSSProperties}
       >
         <img src="./assets/images/icon-calculator.svg" alt="" />
         Calculate Repayments
