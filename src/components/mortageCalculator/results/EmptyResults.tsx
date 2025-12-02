81e3fd00bdff709aab21e24b27f82e20b06a951a
vickbk <vickbake8@gmail.com>
2025-12-02 18:00:10 +0200

    correcting the image path to enable picture loading

--- src/components/mortageCalculator/results/EmptyResults.tsx
+++ src/components/mortageCalculator/results/EmptyResults.tsx
@@ -3,7 +3,7 @@ import { Heading } from "../../shared/Heading";
 export const EmptyResults = () => {
   return (
     <article className="grid gap-4 justify-items-center">
-      <img src="/assets/images/illustration-empty.svg" alt="Empty results" />
+      <img src="./assets/images/illustration-empty.svg" alt="Empty results" />
       <Heading className="text-2xl font-semibold">Results shown here</Heading>
       <p className="text-center c-slate-100">
         Complete the form and click “calculate repayments” to see what your
